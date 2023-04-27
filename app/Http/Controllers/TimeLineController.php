25539f071784a8713925453354a1548a7eab978d
Geriththanan <geriththanan@example.com>
2023-04-27 19:37:22 +0530

    Service Component for TimeLine Initiated

---
+++ app/Http/Controllers/TimeLineController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TimeLine;
+use App\Http\Requests\StoreTimeLineRequest;
+use App\Http\Requests\UpdateTimeLineRequest;
+
+class TimeLineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTimeLineRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TimeLine $timeLine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TimeLine $timeLine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTimeLineRequest $request, TimeLine $timeLine)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TimeLine $timeLine)
+    {
+        //
+    }
+}
